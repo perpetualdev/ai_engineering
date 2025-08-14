15fa81f8b401c47aaf952c6e44a5de6d3d2bf2cd
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2025-08-14 10:26:34 +0100

    fix: :bug: change the numbering to range

--- tasks/week_two_tasks/perpetual_meninwa_task6/football_ticket_system.py
+++ tasks/week_two_tasks/perpetual_meninwa_task6/football_ticket_system.py
@@ -1,5 +1,5 @@
 # Store seat numbers (1 to 50) in a set
-seat_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}
+seat_numbers = set(range(1, 51))
 
 # Ask user to book a seat from 1 - 50
 book_seat = int(input("Book a seat from 1 to 50: "))
