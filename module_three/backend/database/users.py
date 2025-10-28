0a31d923560391fc6daec6ec8884b210b66107a1
perpetualdev <ogochukwumeninwa@gmail.com>
2025-10-28 10:49:55 +0100

    new changes

--- module_three/backend/database/users.py
+++ module_three/backend/database/users.py
@@ -6,20 +6,23 @@ import os
 from dotenv import load_dotenv
 import bcrypt
 import uvicorn
+from middleware import create_token
 
 load_dotenv()
 
 app = FastAPI(title="Simple App", version="1.0.0")
 
-class Simple(BaseModel):
-  name: str = Field(..., example="Sam Larry")
-  email: str = Field(..., example="sam@gmail.com")
-  password: str = Field(..., example="sam123")
-  userType: str = Field(..., example="student")
+expiry_time = int(os.getenv("token_time")) # type: ignore
 
-# class LoginRequest(BaseModel):
-#   email: str = Field(..., example="sam@gmail.com")
-#   password: str = Field(..., example="sam123")
+class Simple(BaseModel):
+  name: str = Field(..., example="Sam Larry") # type: ignore
+  email: str = Field(..., example="sam@gmail.com") # type: ignore
+  password: str = Field(..., example="sam123") # type: ignore
+  userType: str = Field(..., example="student") # type: ignore
+
+class LoginRequest(BaseModel):
+  email: str = Field(..., example="sam@gmail.com") # type: ignore
+  password: str = Field(..., example="sam123") # type: ignore
 
 @app.post("/signup")
 def signUp(input: Simple):
@@ -29,12 +32,10 @@ def signUp(input: Simple):
       WHERE email = :email
     """)
 
-    existing = db.execute(duplicate_query, {"email": input.email})
+    existing = db.execute(duplicate_query, {"email": input.email}).fetchone
 
     if existing:
-      print("Email already exist")
-      return
-      # raise HTTPException(status_code=400, detail="Email already exist!")
+      raise HTTPException(status_code=400, detail="Email already exist!")
 
     query = text("""
       INSERT INTO users (name, email, password, userType)
@@ -66,17 +67,34 @@ def signUp(input: Simple):
     raise HTTPException(status_code=500, detail = str(e))
   
 
-# @app.post('/login')
-# def login(input: LoginRequest):
-#   try:
-#     query = text("""
-#       SELECT * FROM users where email = :email
-#     """)
+@app.post("/login")
+def login(input: LoginRequest):
+  try:
+    query = text("""
+      SELECT * FROM users WHERE email = :email
+    """)
+    result = db.execute(query, {"email": input.email}).fetchone()
 
-#     result = db.execute(query, 
-#             {"email": input.email})
-#   except Exception as e:
-#     raise HTTPException(status_code=404, detail=str(e))
+    if not result:
+      raise HTTPException(status_code=404, detail = "invalid email or password")
+    
+    verified_password = bcrypt.checkpw(input.password.encode('utf-8'), result.password.encode('utf-8'))
+
+    if not verified_password:
+      raise HTTPException(status_code=404, detail = "invalid email or password")
+    
+    encoded_token = create_token({
+      "email": result.email,
+      "userType": result.userType
+    }, expiry_time)
+
+    return {
+      "message": "Login Successful",
+      "token": encoded_token
+    }
+
+  except Exception as e:
+    raise HTTPException(status_code=500, detail = str(e))
   
 if __name__ == "__main__":
-  uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
+  uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port"))) # type: ignore
