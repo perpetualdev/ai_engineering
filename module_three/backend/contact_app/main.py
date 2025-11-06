b9aba60099034bb15dca0e987a1dcbc81ecc9989
Perpy-del <perpetualmeninwa@gmail.com>
2025-11-06 09:23:59 +0100

    new changes

--- module_three/backend/contact_app/main.py
+++ module_three/backend/contact_app/main.py
@@ -15,4 +15,4 @@ def root():
   return {"message": "Welcome to the Contact App"}
 
 if __name__ == "__main__":
-  uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port"))) # type: ignore
+  uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("contact_port"))) # type: ignore
